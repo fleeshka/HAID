2c4fbc0e916e85a68babfcc7865a4d82b7a82478
Ksksksksksksksks <mnenestrashno@gmail.com>
2025-05-10 14:59:23 +0300

    added unit_price function for better understanding

--- api_handler.py
+++ api_handler.py
@@ -1,14 +1,48 @@
 import pandas as pd
 
-df = pd.read_csv("products.csv")
+df = pd.read_csv("data/products.csv")
 
+'''converts default price into suitable for comparison format'''
+def calculate_unit_price(quantity, unit, price):
+    unit = str(unit).strip().lower()
+
+    try:
+        quantity = float(str(quantity).replace(',', '.'))
+    except:
+        return None
+
+    if unit == 'л':
+        q = quantity
+    elif unit == 'мл':
+        q = quantity / 1000
+    elif unit == 'кг':
+        q = quantity
+    elif unit == 'г':
+        q = quantity / 1000
+    elif unit == 'шт':
+        q = quantity
+    elif unit == 'пакетиков':
+        q = quantity
+    elif unit == 'порций - пакетиков по 40 г':
+        q = quantity * 40 / 1000
+    else:
+        return None
+
+    if q == 0:
+        return None
+
+    return round(price / q, 2)
+
+
+'''returns needed products'''
 def get_products(
     store=None,
     category=None,
     min_price=None,
     max_price=None,
     available=True,
-    type = None
+    type = None,
+    need_unit_price = True,
 ):
     filtered = df.copy()
 
@@ -24,8 +58,15 @@ def get_products(
         filtered = filtered[filtered["availability"] == "ИСТИНА"]
     if type:
         filtered = filtered[filtered["product_type"] == type]
+    if need_unit_price:
+        filtered["unit_price"] = filtered.apply(
+            lambda row: calculate_unit_price(row["quantity"], row["unit"], row["price"]),
+            axis=1
+        )
 
-    return filtered.to_dict(orient="records")
+    result = filtered.drop(columns = ['availability', 'category'])
+
+    return result.to_dict(orient="records")
 
 if __name__ == "__main__":
     results = get_products(available=True)
